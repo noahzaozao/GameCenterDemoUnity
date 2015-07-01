4bb26955bfc977143136600c4b51c958e7062ced
noahzao <234082230@qq.com>
2015-07-01 14:14:19 +0800

    update add GooglePlayServices

--- Assets/Scripts/main.cs
+++ Assets/Scripts/main.cs
@@ -1,14 +1,26 @@
-﻿using UnityEngine;
+﻿#if UNITY_IPHONE
+using UnityEngine;
 using System.Collections;
 using UnityEngine.SocialPlatforms;
 using UnityEngine.SocialPlatforms.GameCenter;
+#elif UNITY_ANDROID
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+#else
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SocialPlatforms;
+#endif
 
 public class main : MonoBehaviour {
 
-	public GameCenterPlatform gcp = new GameCenterPlatform ();
+#if UNITY_IPHONE
 	// Use this for initialization
 	void Start () {
-		this.gcp.localUser.Authenticate (onAuthenticate);
+		Social.Active = new GameCenterPlatform ();
+		Social.localUser.Authenticate (onAuthenticate);
 	}
 	
 	// Update is called once per frame
@@ -18,13 +30,7 @@ public class main : MonoBehaviour {
 
 	public void onAuthenticate (bool authed){
 		if (authed) {
-			this.gcp.LoadAchievements (onLoadAchievements);
-		}
-	}
-
-	public void onAuthenticateAndShowAchievementsUI (bool authed){
-		if (authed) {
-			this.gcp.ShowAchievementsUI ();
+			Social.LoadAchievements (onLoadAchievements);
 		}
 	}
 
@@ -32,14 +38,130 @@ public class main : MonoBehaviour {
 		Debug.Log ("onLoadAchievements");
 	}
 
+	public void onAuthenticateAndShowAchievementsUI (bool authed){
+		if (authed) {
+			Social.ShowAchievementsUI ();
+		}
+	}
+
+	public void onAuthenticateAndShowLeaderboardUI (bool authed){
+		if (authed) {
+			Social.ShowLeaderboardUI ();
+		}
+	}
+
 	public void onClickShowAchievementUI () {
 		Debug.Log ("ShowAchievementUI start");
-		if (this.gcp.localUser.authenticated == false) {
-			this.gcp.localUser.Authenticate (onAuthenticateAndShowAchievementsUI);
+		if (Social.localUser.authenticated == false) {
+			Social.localUser.Authenticate (onAuthenticateAndShowAchievementsUI);
 		} else {
 			onAuthenticateAndShowAchievementsUI(true);
 		}
 		Debug.Log ("ShowAchievementUI finished");
 	}
+
+	public void onClickShowLeaderboardUI () {
+		Debug.Log ("ShowLeaderboardUI start");
+		if (Social.localUser.authenticated == false) {
+			Social.localUser.Authenticate (onAuthenticateAndShowLeaderboardUI);
+		} else {
+			onAuthenticateAndShowLeaderboardUI(true);
+		}
+		Debug.Log ("ShowLeaderboardUI finished");
+	}
+#elif UNITY_ANDROID
+	void Awake () {
+		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+			.EnableSavedGames()
+				.WithInvitationDelegate((invitation, shouldAutoAccept) => {})
+				.WithMatchDelegate((match, shouldAutoLaunch) => {})
+				.Build();
+		PlayGamesPlatform.InitializeInstance(config);
+		PlayGamesPlatform.DebugLogEnabled = true;
+		PlayGamesPlatform.Activate();
+	}
+
+	// Use this for initialization
+	void Start () {
+		Social.localUser.Authenticate((bool success) => {
+			onAuthenticate(success);
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void onAuthenticate (bool authed){
+		if (authed) {
+			Social.LoadAchievements (onLoadAchievements);
+		}
+	}
+	
+	public void onLoadAchievements(IAchievement[] items) {
+		Debug.Log ("onLoadAchievements");
+	}
+	
+	public void onAuthenticateAndShowAchievementsUI (bool authed){
+		if (authed) {
+			Social.ShowAchievementsUI ();
+		}
+	}
+	
+	public void onAuthenticateAndShowLeaderboardUI (bool authed){
+		if (authed) {
+			Social.ShowLeaderboardUI ();
+		}
+	}
+	
+	public void onClickShowAchievementUI () {
+		Debug.Log ("ShowAchievementUI start");
+		if (Social.localUser.authenticated == false) {
+			Social.localUser.Authenticate (onAuthenticateAndShowAchievementsUI);
+		} else {
+			onAuthenticateAndShowAchievementsUI(true);
+		}
+		Debug.Log ("ShowAchievementUI finished");
+	}
+	
+	public void onClickShowLeaderboardUI () {
+		Debug.Log ("ShowLeaderboardUI start");
+		if (Social.localUser.authenticated == false) {
+			Social.localUser.Authenticate (onAuthenticateAndShowLeaderboardUI);
+		} else {
+			onAuthenticateAndShowLeaderboardUI(true);
+		}
+		Debug.Log ("ShowLeaderboardUI finished");
+	}
+#else
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void onAuthenticate (bool authed){
+	}
+	
+	public void onLoadAchievements(IAchievement[] items) {
+	}
+	
+	public void onAuthenticateAndShowAchievementsUI (bool authed){
+	}
+	
+	public void onAuthenticateAndShowLeaderboardUI (bool authed){
+	}
+	
+	public void onClickShowAchievementUI () {
+	}
+	
+	public void onClickShowLeaderboardUI () {
+	}
+#endif
 
 }
